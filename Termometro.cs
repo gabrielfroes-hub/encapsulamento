b9fbfe5494773076f5c3fd92023a0e1545528ad0
gabrielfroes-hub <gabriel.froes@edu.senai.br>
2026-09-11 09:49:56 -0300

    exer4

---
+++ Termometro.cs
@@ -0,0 +1,40 @@
+﻿class Termometro
+{
+    private double _temperaturaCelsius;
+
+    public double Celsius
+    {
+        get
+        {
+            return _temperaturaCelsius;
+        }
+        set
+        {
+            _temperaturaCelsius = value;
+        }
+    }
+
+    public double Fahrenheit
+    {
+        get
+        {
+            return _temperaturaCelsius * 9 / 5 + 32;
+        }
+        set
+        {
+            _temperaturaCelsius = (value - 32) * 5 / 9;
+        }
+    }
+
+    public double Kelvin
+    {
+        get
+        {
+            return _temperaturaCelsius + 273.15;
+        }
+        set
+        {
+            _temperaturaCelsius = value - 273.15;
+        }
+    }
+}
